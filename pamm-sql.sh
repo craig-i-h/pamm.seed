f4f5072c0de2767aec63e01a5ae64c396bb46377
europa <andres.alos@ntlworld.com>
2016-05-27 16:54:18 +0100

    fix creation of db objects.

--- pamm-sql.sh
+++ pamm-sql.sh
@@ -26,7 +26,7 @@ MARIA_TAG=latest
 DB_PWD=n3us
 
 # the name of the user to have db access
-DB_USER_NAME="pamm"
+DB_USER_NAME=pamm
 
 # the name of the db to be created
 DB_NAME=pamm
@@ -52,25 +52,25 @@ exited=$(docker ps -a --filter name=$CONTAINER_NAME | grep -c Exited)
 if [[ up -eq 0 ]] && [[ exited -eq 0 ]]; then  
   if [[ ! -d "$CONTAINER_VOLUME_PATH" ]]; then
     echo 'Creating SQL data files location on the host'
-    mkdir $CONTAINER_VOLUME_PATH
+    sudo mkdir ${CONTAINER_VOLUME_PATH}
   fi
   echo 'No container found - creating pamm-sql container'
-  docker run --name $CONTAINER_NAME -v $CONTAINER_VOLUME_PATH:$CONTAINER_VOLUME_PATH -e MYSQL_ROOT_PASSWORD=$DB_PWD -p $DB_PORT:3306 -d mariadb:$MARIA_TAG
+  docker run --name ${CONTAINER_NAME} -v ${CONTAINER_VOLUME_PATH}:${CONTAINER_VOLUME_PATH} -e MYSQL_ROOT_PASSWORD=${DB_PWD} -p ${DB_PORT}:3306 -d mariadb:${MARIA_TAG}
 
   echo 'Creating the PAMM database'
-  a=(docker exec -it $CONTAINER_NAME bash -c "mysql -uroot -p$DB_PWD -e 'create database $DB_NAME;'")
+  a=(docker exec -it ${CONTAINER_NAME} bash -c "mysql -uroot -p${DB_PWD} -e 'create database ${DB_NAME};'")
 
   echo 'Creating the PAMM user'
-  a=(docker exec $CONTAINER_NAME bash -c "mysql -uroot -p$DB_PWD -e 'grant all privileges on $DB_NAME.* to '\''$DB_USER_NAME'\''@'\''%'\'' identified by '\''$DB_PWD'\'';'") 
+  a=(docker exec $CONTAINER_NAME bash -c "mysql -uroot -p${DB_PWD} -e 'grant all privileges on ${DB_NAME}.* to '\''${DB_USER_NAME}'\''@'\''%'\'' identified by '\''${DB_PWD}'\'';'")
 
   echo 'Copying the pamm sql script to the container'
-  docker cp $DB_SCHEMA_PATH $CONTAINER_NAME:/db.sql
+  docker cp ${DB_SCHEMA_PATH} ${CONTAINER_NAME}:/db.sql
 
   echo 'Creating the database tables and referencial integrity'
-  a=(docker exec $CONTAINER_NAME bash -c "mysql -uroot -p$DB_PWD $DB_NAME < db.sql")
+  a=(docker exec ${CONTAINER_NAME} bash -c "mysql -uroot -p${DB_PWD} ${DB_NAME} < db.sql")
 elif [[ exited -eq 0 ]]; then
   echo 'pamm-sql is already running!'
 else
   echo 'pamm-sql exists but is not running - starting pamm-sql'
-  docker start $CONTAINER_NAME
+  docker start ${CONTAINER_NAME}
 fi
